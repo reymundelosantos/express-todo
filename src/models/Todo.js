07598495b2f34e24686b0be4b94168d2887af830
Reymund Delos Santos III <reymundd@codev.com>
2023-08-21 02:10:06 +0800

    Added the todos model

---
+++ src/models/Todo.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const todoSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    description: { type: String },
+    isCompleted: { type: Boolean, default: false },
+    completedAt: { type: Date },
+    createdAt: { type: Date, default: Date.now }
+})
+
+const Todo = mongoose.model('Todo', todoSchema);
+
+export default Todo
